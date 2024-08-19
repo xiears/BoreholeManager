88f65d55288cd2327a0fe2e3c7eba1eba34293e6
Simon Hewitt <simon.hewitt@gmail.com>
2024-08-19 14:43:15 +0100

    Added Add, Delete and further tweaks to Index

--- BoreholeMVCUI/Controllers/BoreholeController.cs
+++ BoreholeMVCUI/Controllers/BoreholeController.cs
@@ -20,5 +20,28 @@ namespace BoreholeMVCUI.Controllers
 
             return View(allBoreholes);
         }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(BoreholeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Add(new BoreholeData.Entities.Borehole() { Depth = model.Depth, X = model.X, Y = model.Y });
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            repository.Delete(id);
+            return RedirectToAction("Index");
+        }
     }
 }
--- BoreholeMVCUI/Models/BoreholeViewModel.cs
+++ BoreholeMVCUI/Models/BoreholeViewModel.cs
@@ -1,13 +1,23 @@
-﻿namespace BoreholeMVCUI.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BoreholeMVCUI.Models
 {
     public class BoreholeViewModel
     {
+        [DisplayName("ID")]
         public int Id { get; set; }
 
+        [DisplayName("X Position")]
+        [Required]
         public int X { get; set; }
 
+        [DisplayName("Y Position")]
+        [Required]
         public int Y { get; set; }
 
+        [DisplayName("Depth")]
+        [Range(0, 500)]
         public decimal Depth { get; set; }
     }
 }

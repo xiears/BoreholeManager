545d5017cea37f5d83898040ceef1df08f609e91
Simon Hewitt <simon.hewitt@gmail.com>
2024-08-19 17:07:01 +0100

    Error handling tweaks/styling

--- BoreholeMVCUI/Models/BoreholeViewModel.cs
+++ BoreholeMVCUI/Models/BoreholeViewModel.cs
@@ -9,15 +9,16 @@ namespace BoreholeMVCUI.Models
         public int Id { get; set; }
 
         [DisplayName("X Position")]
-        [Required]
+        [Required(ErrorMessage = "This field is required")]
         public int X { get; set; }
 
         [DisplayName("Y Position")]
-        [Required]
+        [Required(ErrorMessage = "This field is required")]
         public int Y { get; set; }
 
         [DisplayName("Depth")]
         [Range(0, 500)]
+        [Required(ErrorMessage = "This field is required")]
         public decimal Depth { get; set; }
     }
 }

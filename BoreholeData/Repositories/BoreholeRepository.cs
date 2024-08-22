3de11c1478e50d10f45f46b5beb74f7b19489151
Simon Hewitt <simon.hewitt@gmail.com>
2024-08-22 11:28:34 +0100

    Added further logging to repo

--- BoreholeData/Repositories/BoreholeRepository.cs
+++ BoreholeData/Repositories/BoreholeRepository.cs
@@ -35,29 +35,51 @@ namespace BoreholeData.Repositories
 
         public void Edit (Borehole bh)
         {
-            context.Update (bh);
-            context.SaveChanges();
+            try
+            {
+                logger.LogInformation($"Saving changes to a borehole {bh}");
+
+                context.Update (bh);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Error saving Borehole - {ex}");
+            }
         }
 
         public void Delete (int id)
         {
-            var borehole = GetById(id);
-            context.Remove (borehole);
-            context.SaveChanges();
+            try
+            {
+                logger.LogInformation($"Deleting borehole with id {id}");
+
+                var borehole = GetById(id);
+                context.Remove(borehole);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Error deleting Borehole - {ex}");
+            }
         }
 
         public Borehole GetById(int id)
         {
+            logger.LogInformation($"Getting borehole with id {id}");
+
             return context.Boreholes.Where(bh => bh.ID == id).FirstOrDefault();
         }
 
         public List<Borehole> GetAll()
         {
+            logger.LogInformation($"Getting all boreholes");
             return context.Boreholes.ToList();
         }
 
         public List<Borehole> GetByArea(int x1, int y1, int x2, int y2)
         {
+            logger.LogInformation($"Getting all boreholes in area ({x1},{y1}),({x2},{y2});
             return context.Boreholes.Where(bh => (bh.X >= x1 && bh.X <= x2 && bh.Y >= y1 && bh.Y <= y2)).ToList();
         }
     }
